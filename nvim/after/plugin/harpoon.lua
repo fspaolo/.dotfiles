698321a00def0fa10d444e1317ee87c3a3e7a2ce
fspaolo <fspaolo@gmail.com>
2023-01-25 20:05:40 -0800

    Add tmux configs

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -5,11 +5,11 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>i", mark.add_file)
 vim.keymap.set("n", "<leader>o", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>j", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>k", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>l", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>;", function() ui.nav_file(4) end)
 
-vim.keymap.set("n", "<leader>j", ui.nav_next)
-vim.keymap.set("n", "<leader>k", ui.nav_prev)
+-- vim.keymap.set("n", "<leader>j", ui.nav_next)
+-- vim.keymap.set("n", "<leader>k", ui.nav_prev)
 
